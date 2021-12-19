d4dcd7aaae534da96c649c9f74813ccf492a94e5
stouma915 <sasakitouma@yahoo.ne.jp>
2021-12-19 13:17:39 +0900

    不要なimportを削除

--- src/main/scala/net/stouma915/hydrogenmod/gui/screen/ElectrolyzerScreen.scala
+++ src/main/scala/net/stouma915/hydrogenmod/gui/screen/ElectrolyzerScreen.scala
@@ -10,8 +10,6 @@ import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.player.Inventory
 import net.stouma915.hydrogenmod.gui.menu.ElectrolyzerMenu
 
-import scala.collection.immutable.HashMap
-
 sealed class ElectrolyzerScreen private[gui] (
     container: ElectrolyzerMenu,
     inventory: Inventory,
