3ef318a253c2887ed2cf371759bb3195594a588d
stouma915 <sasakitouma@yahoo.ne.jp>
2022-03-15 14:23:06 +0900

    Update NullCheckSyntax.scala

--- src/main/scala/net/stouma915/hydrogenmod/syntax/NullCheckSyntax.scala
+++ src/main/scala/net/stouma915/hydrogenmod/syntax/NullCheckSyntax.scala
@@ -10,22 +10,4 @@ trait NullCheckSyntax {
 
   }
 
-  implicit class AnyRefListOps[A <: AnyRef](anyRefList: List[A]) {
-
-    def hasNull: Boolean = anyRefList.exists(_.isNull)
-
-  }
-
-  implicit class AnyRefSetOps[A <: AnyRef](anyRefSet: Set[A]) {
-
-    def hasNull: Boolean = anyRefSet.exists(_.isNull)
-
-  }
-
-  implicit class AnyRefSeqOps[A <: AnyRef](anyRefSeq: Seq[A]) {
-
-    def hasNull: Boolean = anyRefSeq.exists(_.isNull)
-
-  }
-
 }
