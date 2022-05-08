49f1179a6bc28a91ebf3d3462321c07013599f48
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-09 08:02:03 +0900

    Update HydrogenModRegistries.scala

--- src/main/scala/net/stouma915/hydrogenmod/init/HydrogenModRegistries.scala
+++ src/main/scala/net/stouma915/hydrogenmod/init/HydrogenModRegistries.scala
@@ -5,10 +5,15 @@ import net.stouma915.hydrogenmod.HydrogenMod
 
 private[hydrogenmod] object HydrogenModRegistries {
 
+  final val BlockRegistry =
+    DeferredRegistry.create(ForgeRegistries.BLOCKS, HydrogenMod.ModId)
+
   final val ItemRegistry =
     DeferredRegister.create(ForgeRegistries.ITEMS, HydrogenMod.ModId)
 
-  def getAllRegistries: Set[DeferredRegister[_]] =
-    Set(ItemRegistry)
+  def getAllRegistries: Set[DeferredRegister[_]] = Set(
+    BlockRegistry,
+    ItemRegistry
+  )
 
 }
