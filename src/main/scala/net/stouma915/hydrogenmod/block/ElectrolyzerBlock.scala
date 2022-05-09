d2e2e3f26b4a367a9d3778b68809c93d8bb74276
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-10 07:57:34 +0900

    Create ElectrolyzerBlock.scala

---
+++ src/main/scala/net/stouma915/hydrogenmod/block/ElectrolyzerBlock.scala
@@ -0,0 +1,19 @@
+package net.stouma915.hydrogenmod.block
+
+import net.minecraft.block.{AbstractBlock, Block}
+import net.minecraft.block.material.{Material, MaterialColor}
+
+object ElectrolyzerBlock {
+
+  private val instance = new ElectrolyzerBlock
+
+  def apply(): Block = instance
+
+}
+
+final class ElectrolyzerBlock private ()
+    extends Block(
+      AbstractBlock
+        .Properties
+        .of(Material.STONE, MaterialColor.STONE)
+    )
