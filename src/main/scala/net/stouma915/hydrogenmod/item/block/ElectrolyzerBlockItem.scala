e59cc870175e9356d1529e575041df463b9e23fb
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-10 08:17:25 +0900

    Create ElectrolyzerBlockItem.scala

---
+++ src/main/scala/net/stouma915/hydrogenmod/item/block/ElectrolyzerBlockItem.scala
@@ -0,0 +1,21 @@
+package net.stouma915.hydrogenmod.item.block
+
+import net.minecraft.item.{BlockItem, Item}
+import net.stouma915.hydrogenmod.block.ElectrolyzerBlock
+import net.stouma915.hydrogenmod.creativetab.HydrogenModTab
+
+object ElectrolyzerBlockItem {
+
+  private val instance = new ElectrolyzerBlockItem
+
+  def apply(): Item = instance
+
+}
+
+final class ElectrolyzerBlockItem private ()
+    extends BlockItem(
+      ElectrolyzerBlock(),
+      new Item
+        .Properties()
+        .tab(HydrogenModTab())
+    )
