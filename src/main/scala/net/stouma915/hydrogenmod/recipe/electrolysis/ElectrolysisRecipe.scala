7fec4fc615c3e978536231f4248443e2b9a3329f
stouma915 <sasakitouma@yahoo.ne.jp>
2022-05-11 16:12:51 +0900

    Update ElectrolysisRecipe.scala

--- src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisRecipe.scala
+++ src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisRecipe.scala
@@ -4,10 +4,29 @@ import net.minecraft.item.ItemStack
 
 trait ElectrolysisRecipe {
 
-  def isInputCorrect(itemStack: ItemStack): Boolean
+  /***
+    * Returns true if inputItem is correct as an input item.
+    * Example: Water bucket
+    *
+    * @param inputItem input item
+    * @return correct or not
+    */
+  def isInputCorrect(inputItem: ItemStack): Boolean
 
+  /***
+    * Items output as result of electrolysis.
+    * Example: 2x Hydrogen and 1x Oxygen
+    *
+    * @return output items
+    */
   def outputItems: List[ItemStack]
 
+  /***
+    * Item to be left in input slot after electrolysis is completed.
+    * Example: Empty bucket
+    *
+    * @return item to leave
+    */
   def itemToLeave: Option[ItemStack] = None
 
 }
