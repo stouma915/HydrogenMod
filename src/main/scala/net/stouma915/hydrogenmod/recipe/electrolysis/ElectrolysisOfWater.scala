c9c6a2070a99dda4cc28aeb9c4da26731974dd27
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-12 08:18:15 +0900

    Update ElectrolysisOfWater.scala

--- src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisOfWater.scala
+++ src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisOfWater.scala
@@ -17,18 +17,15 @@ final class ElectrolysisOfWater private () extends ElectrolysisRecipe {
   override def isInputCorrect(inputItem: ItemStack): Boolean =
     inputItem.getItem eq Items.WATER_BUCKET
 
-  override def outputItems(inputItem: ItemStack): List[ItemStack] =
-    if (isInputCorrect(inputItem))
-      List(
-        {
-          val itemStack = HydrogenItem().toGeneralItemStack
-          itemStack.setCount(2)
-          itemStack
-        },
-        OxygenItem().toGeneralItemStack
-      )
-    else
-      List()
+  override def outputItems(_: ItemStack): List[ItemStack] =
+    List(
+      {
+        val itemStack = HydrogenItem().toGeneralItemStack
+        itemStack.setCount(2)
+        itemStack
+      },
+      OxygenItem().toGeneralItemStack
+    )
 
   override def itemToLeave(inputItem: ItemStack): Option[ItemStack] =
     if (inputItem eq Items.WATER_BUCKET)
