14b69c31b684df8cdd11d8ac24ab9084fcffddd1
stouma915 <sasakitouma@yahoo.ne.jp>
2022-05-11 16:19:54 +0900

    Create ElectrolysisOfWater.scala

---
+++ src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisOfWater.scala
@@ -0,0 +1,32 @@
+package net.stouma915.hydrogenmod.recipe.electrolysis
+
+import net.minecraft.item.{ItemStack, Items}
+import net.stouma915.hydrogenmod.implicits._
+import net.stouma915.hydrogenmod.item.{HydrogenItem, OxygenItem}
+
+object ElectrolysisOfWater {
+
+  private val instance = new ElectrolysisOfWater
+
+  def apply(): ElectrolysisRecipe = instance
+
+}
+
+final class ElectrolysisOfWater private () extends ElectrolysisRecipe {
+
+  override def isInputCorrect(inputItem: ItemStack): Boolean =
+    inputItem.getItem eq Items.WATER_BUCKET
+
+  override def outputItems: List[ItemStack] = List(
+    {
+      val itemStack = HydrogenItem().toGeneralItemStack
+      itemStack.setCount(2)
+      itemStack
+    },
+    OxygenItem().toGeneralItemStack
+  )
+
+  override def itemToLeave: Option[ItemStack] =
+    Some(Items.BUCKET.toGeneralItemStack)
+
+}
