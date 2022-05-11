11b3f0377b74d0d31afb2d8580adfb8e725b1e03
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-11 13:14:24 +0900

    Create ElectrolysisRecipe.scala

---
+++ src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisRecipe.scala
@@ -0,0 +1,13 @@
+package net.stouma915.hydrogenmod.recipe.electrolysis
+
+import net.minecraft.item.ItemStack
+
+trait ElectrolysisRecipe {
+
+  def inputItem: ItemStack
+
+  def outputItems: List[ItemStack]
+
+  def itemToLeave: Option[ItemStack] = None
+
+}
