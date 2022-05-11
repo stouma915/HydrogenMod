9c49e6c04c2f3c37d171b32f1af3346a06749da9
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-12 08:08:52 +0900

    Update ElectrolysisRecipe.scala

--- src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisRecipe.scala
+++ src/main/scala/net/stouma915/hydrogenmod/recipe/electrolysis/ElectrolysisRecipe.scala
@@ -17,16 +17,18 @@ trait ElectrolysisRecipe {
     * Items output as a result of electrolysis.
     * Example: 2x Hydrogen and 1x Oxygen
     *
+    * @param inputItem input item
     * @return output items
     */
-  def outputItems: List[ItemStack]
+  def outputItems(inputItem: ItemStack): List[ItemStack]
 
   /***
     * The item to be left in Input Slot after electrolysis is completed.
     * Example: Empty bucket
     *
+    * @param inputItem input item
     * @return item to leave
     */
-  def itemToLeave: Option[ItemStack] = None
+  def itemToLeave(inputItem: ItemStack): Option[ItemStack] = None
 
 }
