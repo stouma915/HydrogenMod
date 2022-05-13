2ca15ed7da7a117efc20a394d11f34c14ed9ad56
stouma915 <sasakitouma@yahoo.ne.jp>
2022-05-13 18:52:35 +0900

    電気分解装置のloot_tableを追加

--- src/main/scala/net/stouma915/hydrogenmod/block/ElectrolyzerBlock.scala
+++ src/main/scala/net/stouma915/hydrogenmod/block/ElectrolyzerBlock.scala
@@ -2,6 +2,7 @@ package net.stouma915.hydrogenmod.block
 
 import net.minecraft.block.{AbstractBlock, Block}
 import net.minecraft.block.material.{Material, MaterialColor}
+import net.minecraftforge.common.ToolType
 
 object ElectrolyzerBlock {
 
@@ -15,4 +16,7 @@ final class ElectrolyzerBlock private ()
     extends Block(
       AbstractBlock.Properties
         .of(Material.STONE, MaterialColor.STONE)
+        .requiresCorrectToolForDrops()
+        .harvestTool(ToolType.PICKAXE)
+        .strength(1f, 10f)
     )
