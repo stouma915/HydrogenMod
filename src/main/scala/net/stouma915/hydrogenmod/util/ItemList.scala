751ab6f31d8b928b40350cc1f01452b61b2a5cf0
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-05-16 07:54:23 +0900

    Create ItemList.scala

---
+++ src/main/scala/net/stouma915/hydrogenmod/util/ItemList.scala
@@ -0,0 +1,17 @@
+package net.stouma915.hydrogenmod.util
+
+import net.minecraft.item.ItemStack
+
+object ItemList {
+
+  def of(items: List[ItemStack]): ItemList =
+    new ItemList(items)
+
+}
+
+class ItemList private (items: List[ItemStack]) {
+
+  def appended(another: List[ItemStack]): ItemList =
+    ItemList.of(items.appended(another))
+
+}
