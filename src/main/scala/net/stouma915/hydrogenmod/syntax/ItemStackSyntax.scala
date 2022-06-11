0e04c10c9a375d4172db1d78c6f6fefe8c793dc8
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-06-11 17:38:17 +0900

    Update ItemStackSyntax.scala

--- src/main/scala/net/stouma915/hydrogenmod/syntax/ItemStackSyntax.scala
+++ src/main/scala/net/stouma915/hydrogenmod/syntax/ItemStackSyntax.scala
@@ -16,8 +16,7 @@ trait ItemStackSyntax {
     def isOxygenItem: Boolean =
       HydrogenMod.OxygenItems.contains(itemStack.getItem)
 
-    def isAir: Boolean =
-      itemStack != null && itemStack.getItem eq Items.AIR
+    def isAir: Boolean = itemStack.getItem eq Items.AIR
 
   }
 
